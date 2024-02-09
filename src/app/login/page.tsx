ec491ec0913d1b42de3c4fa4ae48a4f8e56466ff
ChebbiMohamedLakhdher <159257301+ChebbiMohamedLakhdher@users.noreply.github.com>
2024-02-09 17:11:33 +0100

    new files and folders

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -52,7 +52,7 @@ export default function LoginPage() {
         
         <label htmlFor="email">email</label>
         <input 
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+        className="p-2 border  border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
             id="email"
             type="text"
             value={user.email}
