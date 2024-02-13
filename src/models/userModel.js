1ae4dee9c8046dfa00ce076544dec3a93f548c3d
ChebbiMohamedLakhdher <159257301+ChebbiMohamedLakhdher@users.noreply.github.com>
2024-02-13 14:37:08 +0100

    new files and folders

---
+++ src/models/userModel.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+    {
+        name:{
+        type : String,
+        required:[true, "Please provide your name"],
+        },
+        email:{
+            type : String,
+        required:[true, "Please provide your email"],
+        unique:true,
+        },
+        password:{
+            type : String,
+        required:[true, "Please provide your password"],
+        },
+        isEmployee:{
+            type: Boolean,
+            default: false,
+        },
+        isAdmin:{
+            type: Boolean,
+            default: false,
+        },
+        forgotPasswordToken: String,
+        forgotPasswordTokenExpiry: Date,
+        verifyToken: String,
+        verifyTokenExpiry: Date,
+
+    }
+)
+const User =mongoose.models.users || mongoose.model
+("User", userSchema);
+export default User;   
