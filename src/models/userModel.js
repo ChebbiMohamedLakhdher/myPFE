b3aa88d7d94e1e587f18cfd6cb0ace0a740e6242
ChebbiMohamedLakhdher <159257301+ChebbiMohamedLakhdher@users.noreply.github.com>
2024-02-14 14:20:36 +0100

    new files and folders

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,34 +1,35 @@
 import mongoose from "mongoose";
+
 const userSchema = new mongoose.Schema(
     {
-        name:{
-        type : String,
-        required:[true, "Please provide your name"],
+        name: {
+            type: String,
+            required: [true, "Please provide your name"]
         },
-        email:{
-            type : String,
-        required:[true, "Please provide your email"],
-        unique:true,
+        email: {
+            type: String,
+            required: [true, "Please provide your email"],
+            unique: true
         },
-        password:{
-            type : String,
-        required:[true, "Please provide your password"],
+        password: {
+            type: String,
+            required: [true, "Please provide your password"]
         },
-        isEmployee:{
+        isEmployee: {
             type: Boolean,
-            default: false,
+            default: false
         },
-        isAdmin:{
+        isAdmin: {
             type: Boolean,
-            default: false,
+            default: false
         },
         forgotPasswordToken: String,
         forgotPasswordTokenExpiry: Date,
         verifyToken: String,
-        verifyTokenExpiry: Date,
-
+        verifyTokenExpiry: Date
     }
-)
-const User =mongoose.models.users || mongoose.model
-("User", userSchema);
-export default User;   
+);
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
