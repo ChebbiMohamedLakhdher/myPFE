61481aefed07aae1252e433ea357a39ee247e20e
ChebbiMohamedLakhdher <159257301+ChebbiMohamedLakhdher@users.noreply.github.com>
2024-02-15 15:10:29 +0100

    new files and folders

--- src/app/Signup/page.tsx
+++ src/app/Signup/page.tsx
@@ -7,8 +7,7 @@ import { toast } from "react-hot-toast";
 
 const SignupPage = () => {
     const router = useRouter();
-    const [error, setError] = useState(false); 
-    const [error2, setError2] = useState(false); 
+    const [error, setError] = useState(false);  
     const [errorMessage, setErrorMessage] = useState('');
     const [user, setUser] = useState({
         email: "",
@@ -52,7 +51,7 @@ const SignupPage = () => {
             <hr />
             <label htmlFor="name">Name</label>
             <input
-                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+                className="p-2 flex-none w-80 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
                 id="name"
                 type="text"
                 value={user.name}
@@ -62,7 +61,7 @@ const SignupPage = () => {
             <label htmlFor="email">Email</label>
             <div className="relative">
                 <input
-                    className={`p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black ${error ? 'border-red-700 border-4' : ''}`}
+                    className={`p-2 border flex-none w-80 border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black ${error ? 'border-red-700 border-4' : ''}`}
                     id="email"
                     type="text"
                     value={user.email}
@@ -82,22 +81,14 @@ const SignupPage = () => {
             <label htmlFor="password">Password</label>
             <div>
             <input
-                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+                className="p-2 border flex-none w-80 border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
                 id="password"
                 type="password"
                 value={user.password}
                 onChange={(e) => setUser({ ...user, password: e.target.value })}
                 placeholder="Password"
-                onBlur={() => setError2(false)}
             />
-            {error2 && (
-                    <div className="absolute top-0 bottom-4 right-0 flex items-center pr-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-red-700 cursor-pointer" viewBox="0 0 20 20" fill="currentColor">
-                            <path fillRule="evenodd" d="M11 14a1 1 0 11-2 0 1 1 0 012 0zM10 2a8 8 0 100 16A8 8 0 0010 2zM9 12a1 1 0 112 0v-5a1 1 0 11-2 0v5z" clipRule="evenodd" />
-                        </svg>
-                        <div className="absolute bg-red-700 text-white rounded-lg p-2 text-sm top-0 left-full ml-2">{errorMessage}</div>
-                    </div>
-                )}
+           
               </div>  
             <button
                 onClick={handleSignup}
