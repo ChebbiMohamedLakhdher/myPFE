8b254a2bcb3a424cebf0cbac8df5c1c411951be5
mahdi-ba02 <mahdibenabdallah21@gmail.com>
2024-02-19 14:48:26 +0100

    new files and folders

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -44,7 +44,7 @@ import Link from "next/link";
     return (
         <div className="static bg-blue-900 flex flex-col items-center justify-center min-h-screen py-2">
             <h1 className="absolute top-20 text-5xl">Login</h1>
-            <label htmlFor="email">Email</label>
+            <label htmlFor="email" >Email</label>
             <div className="relative">
                 <input
                     className={`p-2 flex-none w-80 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black ${error && errorMessage == "User does not exist" ? 'border-red-700 border-4' : ''}`}
@@ -56,13 +56,12 @@ import Link from "next/link";
                     onBlur={() => setError(false)}
                 />
                 {error && errorMessage == "User does not exist" &&(
-                    <div className="absolute top-0 bottom-4 right-0 flex items-center pr-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-red-700 cursor-pointer" viewBox="0 0 20 20" fill="currentColor">
-                            <path fillRule="evenodd" d="M11 14a1 1 0 11-2 0 1 1 0 012 0zM10 2a8 8 0 100 16A8 8 0 0010 2zM9 12a1 1 0 112 0v-5a1 1 0 11-2 0v5z" clipRule="evenodd" />
-                        </svg>
-                        <div className="absolute  bg-red-700 text-white rounded-lg p-2 text-sm top-0 left-full ml-2">{errorMessage}</div>
-                    </div>
-                )}
+                    <><div className="absolute top-3 right-0 flex items-center pr-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-red-700 cursor-pointer" viewBox="0 0 20 20" fill="currentColor">
+                        <path fillRule="evenodd" d="M11 14a1 1 0 11-2 0 1 1 0 012 0zM10 2a8 8 0 100 16A8 8 0 0010 2zM9 12a1 1 0 112 0v-5a1 1 0 11-2 0v5z" clipRule="evenodd" />
+                    </svg>
+                </div><div className="relative bg-red-700 text-white rounded-lg p-2 text-sm bottom-3 left-0 ">{errorMessage}</div></>
+            )}
             </div>
             <label htmlFor="password">Password</label>
             <div className="relative">
@@ -76,13 +75,12 @@ import Link from "next/link";
                 
                 />
                 {error && errorMessage == "Invalid Password" &&(
-                    <div className="absolute top-0 bottom-4 right-0 flex items-center pr-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-red-700 cursor-pointer" viewBox="0 0 20 20" fill="currentColor">
-                            <path fillRule="evenodd" d="M11 14a1 1 0 11-2 0 1 1 0 012 0zM10 2a8 8 0 100 16A8 8 0 0010 2zM9 12a1 1 0 112 0v-5a1 1 0 11-2 0v5z" clipRule="evenodd" />
-                        </svg>
-                        <div className="absolute bg-red-700 text-white rounded-lg p-2 text-sm top-0 left-full ml-2">{errorMessage}</div>
-                    </div>
-                )}
+                   <><div className="absolute top-3 right-0 flex items-center pr-3">
+                   <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-red-700 cursor-pointer" viewBox="0 0 20 20" fill="currentColor">
+                       <path fillRule="evenodd" d="M11 14a1 1 0 11-2 0 1 1 0 012 0zM10 2a8 8 0 100 16A8 8 0 0010 2zM9 12a1 1 0 112 0v-5a1 1 0 11-2 0v5z" clipRule="evenodd" />
+                   </svg>
+               </div><div className="relative bg-red-700 text-white rounded-lg p-2 text-sm bottom-3 left-0 ">{errorMessage}</div></>
+           )}
             </div>
             <button
                 disabled={buttonDisabled || loading}
@@ -95,7 +93,7 @@ import Link from "next/link";
             <Link href="/signup">Signup</Link>
             <div className="relative items-center ">
             {error && errorMessage == "Invalid Token" &&(
-                    <div className="relative top-8 bottom-4 left-2 items-center pr-3 text-white bg-red-700 pb-3 pt-3 pr-3 pl-3 rounded-lg ">
+                    <div className="relative top-8 bottom-4 left-2 items-center pr-3 text-white bg-red-700 pb-3 pt-3  pl-3 rounded-lg ">
                         <h1 className="whitespace-nowrap">Please Check Your Email To Verify Your Account    </h1>
                     </div>
                 )}
