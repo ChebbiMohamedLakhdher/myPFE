62c5cf40d9218adb61f424bda5a92b7d8c928f61
mahdi-ba02 <mahdibenabdallah21@gmail.com>
2024-04-12 13:24:15 +0100

    new files and folders

--- src/app/off_table/off_table.jsx
+++ src/app/off_table/off_table.jsx
@@ -31,52 +31,52 @@ const Off_table = () => {
     fetchUsers();
 }, []);
   return (
-    <div className="container">
+    <div className="container1">
     <h2>Post</h2>
-    <div className="blog-post">
-      <div className="blog-post_img">
+    <div className="blog-post1">
+      <div className="blog-post_img1">
         <img
           src="https://images.unsplash.com/photo-1612287230202-1ff1d85d1bdf?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTU3fHx0ZWNobm9sb2d5fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
           alt=""
         />
       </div>
-      <div className="blog-post_info">
-        <div className="blog-post_date">
+      <div className="blog-post_info1">
+        <div className="blog-post_date1">
           <span>Sagar Developer</span>
           <span>Nov 12 2021</span>
         </div>
-        <h1 className="blog-post_title">Lorem ipsum dolor sit amet.</h1>
-        <p className="blog-post_text">
+        <h1 className="blog-post_title1">Lorem ipsum dolor sit amet.</h1>
+        <p className="blog-post_text1">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores a,
           tempore veniam quasi sint fugiat facilis, facere, amet magnam optio
           velit. Laudantium et temporibus soluta, esse cupiditate aliquid dicta
           accusantium.
         </p>
-        <a href="#" className="blog-post_cta">
+        <a href="#" className="blog-post_cta1">
           Read More
         </a>
       </div>
     </div>
-    <div className="blog-post">
-      <div className="blog-post_img">
+    <div className="blog-post1">
+      <div className="blog-post_img1">
         <img
           src="https://images.unsplash.com/photo-1562813733-b31f71025d54?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y29kaW5nfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
           alt=""
         />
       </div>
-      <div className="blog-post_info">
-        <div className="blog-post_date">
+      <div className="blog-post_info1">
+        <div className="blog-post_date1">
           <span>Sagar Developer</span>
           <span>Dec 25 2021</span>
         </div>
-        <h1 className="blog-post_title">Lorem ipsum dolor sit amet.</h1>
-        <p className="blog-post_text">
+        <h1 className="blog-post_title1">Lorem ipsum dolor sit amet.</h1>
+        <p className="blog-post_text1">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores a,
           tempore veniam quasi sint fugiat facilis, facere, amet magnam optio
           velit. Laudantium et temporibus soluta, esse cupiditate aliquid dicta
           accusantium.
         </p>
-        <a href="#" className="blog-post_cta">
+        <a href="#" className="blog-post_cta1">
           Read More
         </a>
       </div>
